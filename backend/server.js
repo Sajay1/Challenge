2eade9a233580f41490975969f9cb821372f44a3
Sajay1 <sajaysuresh307@gmail.com>
2025-10-08 20:11:28 +0530

    updates : backend

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const mongoose  = require('mongoose')
+const cors = require('cors')
+const shortURL = require('shorturl')
+const validURL = require('valid-url')
+
+const app = express()
+app.use(cors())
+
+mongoose.connect("mongodb://localhost:27017/challenge")
+
+const urlSchema = mongoose.Schema(
+    {
+    {
+        
+    },
+    {
+
+    },
+}
+)
+
+
+app.get('/shorten',async(req,res)=>{
+
+})
+
+app.post(':/shortCode',async(req,res)=>{
+
+})
+
+
+module.exports = app
