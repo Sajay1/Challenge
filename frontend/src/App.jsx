63699ca6ccd700380035e1905d0418975b2766e0
Sajay1 <sajaysures307@gmail.com>
2025-10-08 19:41:48 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import URL from './URL'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+return(
+  <URL/>
+)
+
+}
+export default App
